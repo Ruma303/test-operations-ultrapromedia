4a32e86e4d87048f781f2c70751a4f68146f59bc
Ruma303 <matteoh8@hotmail.it>
2024-05-16 10:27:06 +0200

    colonna disponibilità vuota

--- index.py
+++ index.py
@@ -7,25 +7,27 @@ headers = {'User-Agent': 'Mozilla/5.0'}
 
 prodotti = []
 
-def estrai_dati_prodotto(prodotto_html):
-    nome = prodotto_html.find('a', class_='product-item-link').text.strip() if prodotto_html.find('a', class_='product-item-link') else "Nome non disponibile"
-    codice = prodotto_html.find('span', style="font-size:inherit;").text.strip() if prodotto_html.find('span', style="font-size:inherit;") else "Codice non disponibile"
+def estrai_dati_prodotto(prodotto):
+    nome = prodotto.find('a', class_='product-item-link').text.strip() if prodotto.find('a', class_='product-item-link') else "Nome non disponibile"
+    codice = prodotto.find('span', style="font-size:inherit;").text.strip() if prodotto.find('span', style="font-size:inherit;") else "Codice non disponibile"
 
-    descrizione_div = prodotto_html.find('div', class_='product-item-description')
+    descrizione_div = prodotto.find('div', class_='product-item-description')
     descrizione_items = descrizione_div.find_all('li') if descrizione_div else []
     descrizione = " | ".join([item.text.strip() for item in descrizione_items]) if descrizione_items else "Descrizione non disponibile"
 
-    # Estrai il prezzo
-    prezzo_tag = prodotto_html.find('span', class_='price')
+    prezzo_tag = prodotto.find('span', class_='price')
     prezzo = prezzo_tag.text.strip() if prezzo_tag else "Prezzo non disponibile"
 
-    # Estrai il link foto
-    img_tag = prodotto_html.find('img', class_='product-image-photo')
+    img_tag = prodotto.find('img', class_='product-image-photo')
     link_foto = img_tag['src'] if img_tag else "Immagine non disponibile"
 
-    # Estrai la disponibilità
-    disponibilita_tag = prodotto_html.find('p', class_='in-stock_add_cart')
-    disponibilita = disponibilita_tag.text.strip() if disponibilita_tag else "Disponibilità non disponibile"
+    disponibilita_tag = prodotto.find('div', class_='availibility')
+    if disponibilita_tag:
+        p_tags = disponibilita_tag.find_all('p')
+        disponibilita_texts = [p.text.strip() for p in p_tags]
+        disponibilita = " | ".join(disponibilita_texts)
+    else:
+        disponibilita = "Disponibilità non disponibile"
 
     return {
         'Codice': codice,
@@ -40,8 +42,8 @@ def scrape_page(url):
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
     prodotti_html = soup.find_all('div', class_='product-item-info')
-    for prodotto_html in prodotti_html:
-        prodotti.append(estrai_dati_prodotto(prodotto_html))
+    for prodotto in prodotti_html:
+        prodotti.append(estrai_dati_prodotto(prodotto))
     next_page = soup.find('a', class_='action next')
     if next_page and 'href' in next_page.attrs:
         scrape_page(next_page['href'])
